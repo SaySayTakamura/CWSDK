60f6ead350ee158fdbf03a58f431f6f6aad04627
Edgar Takamura <nightcorebrasil280@gmail.com>
2024-05-30 19:44:37 -0300

    Fixed compile crash with Class Inclusion
    Just remove the translation unit or comment it if you want and move all function definitions to the class itself, making it self-containing.
    
    Signed: Edgar <3

--- modhelper/CraftingManager/CraftingManager.cpp
+++ modhelper/CraftingManager/CraftingManager.cpp
@@ -1,22 +1,7 @@
-#include "CraftingManager.h"
-
-void modhelper::CraftingManager::CreateCraft(cube::Item* item_result, std::vector<cube::ItemStack>* required_items)
-{
-	cube::Item item;
-	item.Copy(item_result);
-	std::vector<cube::ItemStack> items = {};
-	for (cube::ItemStack stack : *required_items) {
-		items.push_back(stack);
-	}
-	this->crafts.insert({ item, items });
-}
-
-std::vector<cube::ItemStack>* modhelper::CraftingManager::GetCraft(cube::Item* item_result)
-{
-	cube::Item item;
-	item.Copy(item_result);
-	auto search = this->crafts.find(item);
-	if (search != this->crafts.end())
-		return &search->second;
-	return nullptr;
-}
+//#include "CraftingManager.h"
+//
+//void modhelper::CraftingManager::CreateCraft(cube::Item* item_result, std::vector<cube::ItemStack>* required_items)
+//
+//
+//std::vector<cube::ItemStack>* modhelper::CraftingManager::GetCraft(cube::Item* item_result)
+//
--- modhelper/CraftingManager/CraftingManager.h
+++ modhelper/CraftingManager/CraftingManager.h
@@ -6,10 +6,27 @@ namespace modhelper
 {
 	class CraftingManager {
 	private:
-		std::map<cube::Item, std::vector<cube::ItemStack>> crafts;
+		std::map<cube::Item*, std::vector<cube::ItemStack>> crafts;
 	public:
 		CraftingManager() {};
-		void CreateCraft(cube::Item* item_result, std::vector<cube::ItemStack>* required_items);
-		std::vector<cube::ItemStack>* GetCraft(cube::Item* item);
+		void CreateCraft(cube::Item* item_result, std::vector<cube::ItemStack>* required_items)
+		{
+			cube::Item* item;
+			item->Copy(item_result);
+			std::vector<cube::ItemStack> items = {};
+			for (cube::ItemStack stack : *required_items) {
+				items.push_back(stack);
+			}
+			this->crafts.insert({ item, items });
+		}
+		std::vector<cube::ItemStack>* GetCraft(cube::Item* item_result)
+		{
+			cube::Item* item;
+			item->Copy(item_result);
+			auto search = this->crafts.find(item);
+			if (search != this->crafts.end())
+				return &search->second;
+			return nullptr;
+		}
 	};
-};
+}
