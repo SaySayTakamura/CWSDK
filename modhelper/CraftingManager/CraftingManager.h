331871a95364403755277db41f87b2bea62adc74
ToufouMaster <owen.kala@gmx.fr>
2024-05-31 02:13:04 +0200

    Fixed the crash

--- modhelper/CraftingManager/CraftingManager.h
+++ modhelper/CraftingManager/CraftingManager.h
@@ -6,26 +6,24 @@ namespace modhelper
 {
 	class CraftingManager {
 	private:
-		std::map<cube::Item*, std::vector<cube::ItemStack>> crafts;
+		std::map<cube::Item*, std::vector<cube::ItemStack>> crafts = {};
 	public:
 		CraftingManager() {};
 		void CreateCraft(cube::Item* item_result, std::vector<cube::ItemStack>* required_items)
 		{
-			cube::Item* item;
-			item->Copy(item_result);
-			std::vector<cube::ItemStack> items = {};
+			/*std::vector<cube::ItemStack> items = {};
 			for (cube::ItemStack stack : *required_items) {
 				items.push_back(stack);
-			}
-			this->crafts.insert({ item, items });
+			}*/
+			this->crafts.insert({ item_result, *required_items });
 		}
 		std::vector<cube::ItemStack>* GetCraft(cube::Item* item_result)
 		{
-			cube::Item* item;
-			item->Copy(item_result);
-			auto search = this->crafts.find(item);
-			if (search != this->crafts.end())
-				return &search->second;
+			for (std::map<cube::Item*, std::vector<cube::ItemStack>>::iterator it = this->crafts.begin(); it != this->crafts.end(); ++it) {
+				if (item_result->IsItemEqual(it->first)) {
+					return &it->second;
+				}
+			}
 			return nullptr;
 		}
 	};
